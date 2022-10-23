7b6856dec9e6e2af20cd850454c68292e45f25d6
innazhurba <inazhurba@gmail.com>
2022-10-23 12:20:00 +0300

    added: permissions output, showing output data as a table

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <iomanip>
 
 // implementation of the ls -il command in C++ using the dirent.h library and stat.h library
 // to get the inode number, the file size of the file, owner and other data
+
+char* permissions(mode_t perm){
+    auto modeval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+    modeval[0] = (perm & S_IRUSR) ? 'r' : '-';
+    modeval[1] = (perm & S_IWUSR) ? 'w' : '-';
+    modeval[2] = (perm & S_IXUSR) ? 'x' : '-';
+    modeval[3] = (perm & S_IRGRP) ? 'r' : '-';
+    modeval[4] = (perm & S_IWGRP) ? 'w' : '-';
+    modeval[5] = (perm & S_IXGRP) ? 'x' : '-';
+    modeval[6] = (perm & S_IROTH) ? 'r' : '-';
+    modeval[7] = (perm & S_IWOTH) ? 'w' : '-';
+    modeval[8] = (perm & S_IXOTH) ? 'x' : '-';
+    modeval[9] = '\0';
+    return modeval;
+}
 
 int main(int argc, char *argv[]) {
     DIR *dir;
@@ -16,11 +32,17 @@ int main(int argc, char *argv[]) {
     }
     if (dir != NULL) {
 
-        std::cout << "inode number | file size | ownerID | groupID | file name" << std::endl;
+        std::cout << "permisions | file size | ownerID | groupID | inode number | time | file name" << std::endl;
         while ((ent = readdir(dir)) != NULL) {
             stat(ent->d_name, &buf);
+            //show owner name by using getpwuid or ownerID
+
+
             // inode number, file size, ownerID, groupID, file name
-            std::cout << buf.st_ino << " " << buf.st_size << " " << buf.st_uid << " " << buf.st_gid << " " << buf.st_mode << " " << buf << ent->d_name << std::endl;
+            std::cout << std::setw(10) << std::left << permissions(buf.st_mode)  << std::setw(10) << std::left  << buf.st_size
+                      << std::setw(10) << std::left  << buf.st_uid << std::setw(10) << std::left  << buf.st_gid
+                      << std::setw(10) << std::left  << buf.st_ino << std::setw(10) << std::left  << buf.st_ctimespec.tv_sec
+                      << std::setw(22) << std::right << ent->d_name << std::endl;
         }
         closedir(dir);
     } else {
@@ -28,21 +50,3 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
-
-/*int main(int argc, char *argv[])
-{
-    DIR *dir;
-    struct dirent *ent;
-    struct stat buf;
-    if ((dir = opendir (argv[1])) != NULL) {
-        while ((ent = readdir (dir)) != NULL) {
-            stat(ent->d_name, &buf);
-            std::cout << ent->d_ino << " " << buf.st_size << " " << ent->d_name << std::endl;
-        }
-        closedir (dir);
-    } else {
-        perror ("");
-        return EXIT_FAILURE;
-    }
-    return 0;
-}*/
