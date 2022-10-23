4190199f8026af7a60188cb363a4b1d95241b613
innazhurba <inazhurba@gmail.com>
2022-10-23 12:45:31 +0300

    added: data and time of the last modifications, file name output by knowing if it`s the dirrectory/exec/etc

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <iomanip>
+#include <pwd.h>
+#include <grp.h>
 
 // implementation of the ls -il command in C++ using the dirent.h library and stat.h library
 // to get the inode number, the file size of the file, owner and other data
@@ -21,6 +23,76 @@ char* permissions(mode_t perm){
     return modeval;
 }
 
+//return owner of the file by using the ownerID
+char* owner(uid_t ownerID){
+    struct passwd *pw = getpwuid(ownerID);
+    return pw->pw_name;
+}
+
+//return group of the file by using the groupID
+char* group(gid_t groupID){
+    struct group *gr = getgrgid(groupID);
+    return gr->gr_name;
+}
+
+//return file size in bytes
+char* size(off_t size){
+    auto sizeval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+    sprintf(sizeval, "%ld", size);
+    return sizeval;
+}
+
+//return data of the last modification of the file in format YYYY-MM-DD
+char* data(time_t time){
+    auto timeval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+    strftime(timeval, 20, "%Y-%m-%d", localtime(&time));
+    return timeval;
+}
+
+//return time of the last modification of the file in format HH:MM:SS
+char* time(time_t time){
+    auto timeval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+    strftime(timeval, 20, "%H:%M:%S", localtime(&time));
+    return timeval;
+}
+
+//return if file is a directory, file or executable file
+char* type(mode_t type){
+    if (S_ISDIR(type)) return "d";
+    if (S_ISREG(type)) return "-";
+    if (S_ISLNK(type)) return "l";
+    if (S_ISCHR(type)) return "c";
+    if (S_ISBLK(type)) return "b";
+    if (S_ISFIFO(type)) return "p";
+    if (S_ISSOCK(type)) return "s";
+    return "?";
+}
+
+//return name of the file or directory by knowing its type
+// if it`s dirrectory, then add "/" at the begging of the name
+// if it`s executable file, then add "*" at the begging of the name
+char* name(mode_t type, char* name){
+    if (S_ISDIR(type)) {
+        auto nameval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+        sprintf(nameval, "/%s", name);
+        return nameval;
+    }
+    if (S_ISREG(type)) {
+        if (type & S_IXUSR) {
+            auto nameval = static_cast<char *>(malloc(sizeof(char) * 9 + 1));
+            sprintf(nameval, "*%s", name);
+            return nameval;
+        }
+        return name;
+    }
+    if (S_ISLNK(type)) return name;
+    if (S_ISCHR(type)) return name;
+    if (S_ISBLK(type)) return name;
+    if (S_ISFIFO(type)) return name;
+    if (S_ISSOCK(type)) return name;
+    return "?";
+}
+
 int main(int argc, char *argv[]) {
     DIR *dir;
     struct dirent *ent;
@@ -31,18 +103,15 @@ int main(int argc, char *argv[]) {
         dir = opendir(argv[1]);
     }
     if (dir != NULL) {
-
-        std::cout << "permisions | file size | ownerID | groupID | inode number | time | file name" << std::endl;
         while ((ent = readdir(dir)) != NULL) {
             stat(ent->d_name, &buf);
-            //show owner name by using getpwuid or ownerID
-
-
-            // inode number, file size, ownerID, groupID, file name
-            std::cout << std::setw(10) << std::left << permissions(buf.st_mode)  << std::setw(10) << std::left  << buf.st_size
-                      << std::setw(10) << std::left  << buf.st_uid << std::setw(10) << std::left  << buf.st_gid
-                      << std::setw(10) << std::left  << buf.st_ino << std::setw(10) << std::left  << buf.st_ctimespec.tv_sec
-                      << std::setw(22) << std::right << ent->d_name << std::endl;
+            //show all data about file lilke ls -l command
+            std::cout << std::setw(10) << std::left << permissions(buf.st_mode)   //permissions
+                        << std::setw(12) << std::left  << owner(buf.st_uid)             //owner
+                        << std::setw(8) << std::right  <<  size(buf.st_size ) << " "                //file size in bytes
+                        << std::setw(12) << std::left << data(buf.st_mtime)                 //data of the last modification of the file
+                        << std::setw(12) << std::left  << time(buf.st_mtime)             //time of the last modification of the file
+                        << std::setw(12) << std::left << name(buf.st_mode, ent->d_name) << std::endl;      //file name //ent->d_name
         }
         closedir(dir);
     } else {
